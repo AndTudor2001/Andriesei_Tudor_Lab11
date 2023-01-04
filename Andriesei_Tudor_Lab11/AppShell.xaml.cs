6e7103c1cedd02d5f4276df29041dea3900ce0d4
Tudor Andriesei <andrieseitudor@yahoo.com>
2023-01-04 20:12:20 +0200

    Add project files.

---
+++ Andriesei_Tudor_Lab11/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Andriesei_Tudor_Lab11
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Andriesei_Tudor_Lab11/Data/IRestService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Andriesei_Tudor_Lab11.Models;
+
+namespace Andriesei_Tudor_Lab11.Data
+{
+    public interface IRestService
+    {
+        Task <List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
